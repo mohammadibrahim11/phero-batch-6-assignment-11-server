76140906a6eec73c46e56f741f986e7f40c705a1
Mohammad Ibrahim <iibrahim01081998@gmail.com>
2022-11-09 01:59:50 +0600

    post add services

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const cors = require("cors");
-const { MongoClient, ServerApiVersion } = require("mongodb");
-require('dotenv').config()
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
+require("dotenv").config();
 
 const app = express();
 const port = process.env.PORT || 5000;
@@ -10,33 +10,53 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
-const uri =
-  `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.gbplfqy.mongodb.net/?retryWrites=true&w=majority`;
-  console.log(uri);
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.gbplfqy.mongodb.net/?retryWrites=true&w=majority`;
+console.log(uri);
 const client = new MongoClient(uri, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
   serverApi: ServerApiVersion.v1,
 });
 
-async function run(){
-  try{
+async function run() {
+ 
+  try {
 
-    app.get('/services', async(req,res)=>{
-      const serviceCollection = client.db('cleaningService').collection('services');
+    const serviceCollection = client.db("cleaningService").collection("services");
+    const reviewsCollection = client.db('cleaningService').collection('reviews');
+
+    app.get("/services", async (req, res) => {
       const query = {};
       const cursor = serviceCollection.find(query);
-      const services  = await cursor.toArray();
+      const services = await cursor.toArray();
       res.send(services);
-    })
-   
-  }
-  finally{
+    });
 
+    app.get("/servicesHome", async (req, res) => {
+      const query = {};
+      const cursor = serviceCollection.find(query);
+      const services = await cursor.limit(3).toArray();
+      res.send(services);
+    });
+
+
+    app.get('/services/:id', async (req,res)=>{
+      const id = req.params.id;
+      const query = {_id:ObjectId(id)}
+      const serviceDetails = await serviceCollection.findOne(query);
+      res.send(serviceDetails);
+    });
+
+
+    app.post('/reviews', async(req,res)=>{
+      const review = req.body;
+      const result = await reviewsCollection.insertOne(review);
+      res.send(result);
+    })
+  } finally {
   }
 }
 run().catch((error) => console.log(error));
-
 
 // client.connect((err) => {
 //   const collection = client.db("test").collection("devices");
