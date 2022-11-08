e6ba5f7a04f0c5cc536e32703922e8fe5ad8deea
Mohammad Ibrahim <iibrahim01081998@gmail.com>
2022-11-08 13:54:09 +0600

    data loaded from mongodb

--- index.js
+++ index.js
@@ -1,11 +1,48 @@
 const express = require("express");
 const cors = require("cors");
+const { MongoClient, ServerApiVersion } = require("mongodb");
+require('dotenv').config()
+
 const app = express();
 const port = process.env.PORT || 5000;
 
 // middleware
 app.use(cors());
 app.use(express.json());
+
+const uri =
+  `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.gbplfqy.mongodb.net/?retryWrites=true&w=majority`;
+  console.log(uri);
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverApi: ServerApiVersion.v1,
+});
+
+async function run(){
+  try{
+
+    app.get('/services', async(req,res)=>{
+      const serviceCollection = client.db('cleaningService').collection('services');
+      const query = {};
+      const cursor = serviceCollection.find(query);
+      const services  = await cursor.toArray();
+      res.send(services);
+    })
+   
+  }
+  finally{
+
+  }
+}
+run().catch((error) => console.log(error));
+
+
+// client.connect((err) => {
+//   const collection = client.db("test").collection("devices");
+//   // perform actions on the collection object
+//   client.close();
+// });
 
 app.get("/", (req, res) => {
   res.send("simple node server running");
