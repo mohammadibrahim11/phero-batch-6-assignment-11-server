079cf836f0a4e84795c9ed5fa4a3639558497a0a
Mohammad Ibrahim <iibrahim01081998@gmail.com>
2022-11-10 22:05:08 +0600

    remove jwt token

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 const express = require("express");
-const cors = require("cors");
-// const jwt =require('jsonwebtoken');
+const cors = require("cors")
 const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 require("dotenv").config();
 
@@ -11,8 +10,6 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
-
-
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.gbplfqy.mongodb.net/?retryWrites=true&w=majority`;
 console.log(uri);
 const client = new MongoClient(uri, {
@@ -21,41 +18,18 @@ const client = new MongoClient(uri, {
   serverApi: ServerApiVersion.v1,
 });
 
-//  function verifyJWT(req,res,next) {
-//   const authHeader =req.headers.authorization;
-//   if(!authHeader){
-//   return  res.status(401).send({message: 'unauthorized access'});
-//   }
-//   const token = authHeader.split(' ')[1];
-//   jwt.verify(token,process.env.ACCESS_TOKEN_SECRET,function(error,decoded){
-//     if(error){
-//     return  res.status(403).send(
-//         {message:'unauthrized access'}) 
-//          }
-//        req.decoded = decoded
-//          next();
-//   })
-//   // next()
-  
-// }
+
+
+
 
 async function run() {
- 
   try {
-
-    const serviceCollection = client.db("cleaningService").collection("services");
-    const reviewsCollection = client.db('cleaningService').collection('reviews');
-    
-
-    // app.post('/jwt',(req,res)=>{
-    //   const user = req.body;
-    //   console.log(user);
-    //   const token = jwt.sign(user,process.env.ACCESS_TOKEN_SECRET,{expiresIn:'5s'})
-    //   res.send({token})
-    // })
-
-
-
+    const serviceCollection = client
+      .db("cleaningService")
+      .collection("services");
+    const reviewsCollection = client
+      .db("cleaningService")
+      .collection("reviews");
 
     app.get("/services", async (req, res) => {
       const query = {};
@@ -64,18 +38,18 @@ async function run() {
       res.send(services);
     });
 
-    app.get('/services', async(req,res)=>{
-    const query = {};
-    const cursor = serviceCollection.find(query).sort({'_id': -01});
-    const services=await cursor.toArray();
-    res.send(services);
-    })
+    app.get("/services", async (req, res) => {
+      const query = {};
+      const cursor = serviceCollection.find(query).sort({ _id: -01 });
+      const services = await cursor.toArray();
+      res.send(services);
+    });
 
-    app.post('/services', async(req,res)=>{
+    app.post("/services", async (req, res) => {
       const service = req.body;
-      const result = await serviceCollection.insertOne(service)
+      const result = await serviceCollection.insertOne(service);
       res.send(result);
-    })
+    });
 
     app.get("/servicesHome", async (req, res) => {
       const query = {};
@@ -84,81 +58,66 @@ async function run() {
       res.send(services);
     });
 
-
-    app.get('/services/:id', async (req,res)=>{
+    app.get("/services/:id", async (req, res) => {
       const id = req.params.id;
-      const query = {_id:ObjectId(id)}
+      const query = { _id: ObjectId(id) };
       const serviceDetails = await serviceCollection.findOne(query);
       res.send(serviceDetails);
     });
 
-    // app.post('/jwt',(req,res)=>{
-    //   const user = req.body;
-    //   const token=jwt.sign(user,process.env.ACCESS_TOKEN_SECRET, {expiresIn:'1h'});
-    //   res.send({token});
-    // })
-
-    app.get('/reviews', async(req,res)=>{
-     
-    //  const  decoded = req.decoded ;
-    //   console.log('inside api',decoded);
-    //   if(decoded.email !== req.query.email){
-    //     res.status(403).send({message:'forbidden access'})
-    //   }
-
+    app.get("/reviews", async (req, res) => {
       let query = {};
-      if(req.query.email){
-        query={
-          email:req.query.email
-        }
-      }
+      // if(req.query.email){
+      //   query={
+      //     email: req.query.email
+      //   }
+      // }
       const cursor = reviewsCollection.find(query);
       const review = await cursor.toArray();
       res.send(review);
-    })
+    });
 
-    app.get('/reviews/:id',async(req,res)=>{
+    app.get("/reviews/:id", async (req, res) => {
       console.log(req.params.id);
-      const cursor = reviewsCollection.find({service: req.params.id})
-      const reviews = await cursor.toArray()
-      res.json(reviews)
-    })
+      const cursor = reviewsCollection.find({ service: req.params.id });
+      const reviews = await cursor.toArray();
+      res.json(reviews);
+    });
 
-
-    app.post('/reviews', async(req,res)=>{
+    app.post("/reviews", async (req, res) => {
       const review = req.body;
       const result = await reviewsCollection.insertOne(review);
       res.send(result);
     });
 
-    app.patch('/reviews/:id',async(req,res)=>{
+    app.patch("/reviews/:id", async (req, res) => {
       const id = req.params.id;
-      const filter = {_id : ObjectId(id)};
+      const filter = { _id: ObjectId(id) };
       const reviews = req.body;
-      const option = {upsert:true};
-      const updateReview ={
-        $set : {
-          name:reviews.nmae,
-          text : reviews.text,
+      const option = { upsert: true };
+      const updateReview = {
+        $set: {
+          name: reviews.nmae,
+          text: reviews.text,
           photoUrl: reviews.photoUrl,
-        }
-      }
-      const result = await reviewsCollection.updateOne(filter,updateReview,option);
+        },
+      };
+      const result = await reviewsCollection.updateOne(
+        filter,
+        updateReview,
+        option
+      );
       console.log(result);
-      res.send(result)
+      res.send(result);
+    });
 
-    })
-
-    app.delete('/reviews/:id',async(req,res)=>{
+    app.delete("/reviews/:id", async (req, res) => {
       const id = req.params.id;
-      const query = {_id:ObjectId(id)};
+      const query = { _id: ObjectId(id) };
       const result = await reviewsCollection.deleteOne(query);
       res.send(result);
-    })
-
-
-  } 
-  finally {
+    });
+  } finally {
   }
 }
 run().catch((error) => console.log(error));
